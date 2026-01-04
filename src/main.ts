c0a37703733f9b1df54e3b17458daf3a5c4dbe6f
Luong <ctpluong@gmail.com>
2026-01-04 15:34:00 +0700

    complete app

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,19 @@
+// main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { json, urlencoded } from 'express';
 
 async function bootstrap() {
-  const PORT = process.env.PORT ?? 3020;
   const app = await NestFactory.create(AppModule, {
     rawBody: true,
   });
-  // ensure rawBody is available while still parsing for normal routes
-  app.use(urlencoded({ extended: true }));
-  app.use(json({}));
-  await app.listen(PORT);
-  console.log(`Server is running on port ${PORT}`);
-}
 
+  // Cho phép FE localhost truy cập
+  app.enableCors({
+    origin: 'http://localhost:3000', // hoặc '*'
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: '*',
+  });
+
+  await app.listen(process.env.PORT || 3020);
+}
 bootstrap();
